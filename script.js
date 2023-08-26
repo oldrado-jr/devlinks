066803fcb7dfdcdfd104ab5423db2c42f0a738c7
Oldrado Junior <oldradojunior@gmail.com>
2023-08-26 16:22:22 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.querySelector("#switch").addEventListener("click", () => {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos escuros e casaco preto, barba e fundo azul."
+    );
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    );
+  }
+});
